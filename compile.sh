4a4e695737a3c67c4fd1785699cdc0df3a9710e0
AlePalu <alessandropalummo@gmail.com>
2019-09-06 12:37:49 +0200

    compile modified

--- compile.sh
+++ compile.sh
@@ -2,8 +2,10 @@
 
 gradle clean
 gradle NetworkJar
+gradle PaxosJarTestNet
 gradle PaxosJarTestNaming
 gradle PaxosJarFaultTestNaming
 gradle PaxosJarTestProp1
 gradle PaxosJarTestProp2
 gradle PaxosJarTestNoProp
+
